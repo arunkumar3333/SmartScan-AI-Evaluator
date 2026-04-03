ad8d3e200627fc3ec2bd7e03b1fc75f077859d12
arunkumar3333 <arungowdav3@gmail.com>
2026-04-03 12:42:41 +0530

    Hlo

--- backend/src/main/java/com/smartscan/backend/service/processing/ImageProcessingService.java
+++ backend/src/main/java/com/smartscan/backend/service/processing/ImageProcessingService.java
@@ -18,16 +18,16 @@ public class ImageProcessingService {public File preprocess(File file) {
         throw new RuntimeException("Failed to load image: " + file.getAbsolutePath());
     }
 
-    // ✅ 1. Grayscale
+    // 1. Grayscale
     Imgproc.cvtColor(img, img, Imgproc.COLOR_BGR2GRAY);
 
-    // ✅ 2. VERY LIGHT blur (reduce noise only)
+    // 2. VERY LIGHT blur (reduce noise only)
     Imgproc.GaussianBlur(img, img, new Size(3, 3), 0);
 
-    // ✅ 3. Resize (MOST IMPORTANT)
+    //3. Resize (MOST IMPORTANT)
     Imgproc.resize(img, img, new Size(img.width() * 2, img.height() * 2));
 
-    // ❌ REMOVE threshold completely
+    //  REMOVE threshold completely
 
     // Save image
     String outputPath = System.getProperty("user.dir") + "/uploads/processed_" + System.currentTimeMillis() + ".png";
